c83d82e53d227eae3c5f3a5965480539ef4d7554
sarobinson2001 <sarobinson2011@gmail.com>
2023-12-17 15:55:11 +0000

    update deploy_lockdrop.py

--- scripts/deploy_lockdrop.py
+++ scripts/deploy_lockdrop.py
@@ -9,7 +9,7 @@ DEPTH = 5
 GAS_LIMIT = 6000000
 
 def view_storage_slots(_depth, _target):
-    print(f"\n")
+    # print(f"\n")
     for i in range(_depth):
         store_value = web3.eth.get_storage_at(_target, i)
         store_value_hex = web3.toHex(store_value)
@@ -21,9 +21,9 @@ def main():
 
     player = get_account()
     deployment = LockDrop.deploy({"from": player})
-    target = interface.ILockDrop(deployment)
+    # target = interface.ILockDrop(deployment)
     
-    # deployment.deposit({"from": player, "value": Web3.toWei(0.1, "ether")})
+    deployment.deposit({"from": player, "value": Web3.toWei(0.1, "ether")})
     # deployment.withdraw({"from": player})
         
     view_storage_slots(DEPTH, deployment.address) 
